a3b65e78e2eff30a4e7e4d71612931039e44fdc0
zach <zach@134.241.147.191>
2018-03-04 16:41:07 -0500

    Update README.md

--- src/target/Target.java
+++ src/target/Target.java
@@ -1,6 +1,11 @@
 /*==============================================================================
-
-==============================================================================*/
+ Zachary Hadjah                    February 9th 2018
+ Junior Year                       Side Project
+  
+This program first displays a bullseye created by three different sized cirlces.
+Once the animate me button is clicked, the function animation() will make the existing
+circles shrink inwards untill the size of the cirlces are zero.
+ =============================================================================*/
 package target;
 
 import javafx.animation.ParallelTransition;
@@ -21,24 +26,21 @@ import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
-/**
- *
- * @author zach
- */
-public class BullsEye extends Application 
+public class Target extends Application 
 {
     Circle[] cir = new Circle[7];
     Button btn = new Button("Animate me!");
     StackPane root = new StackPane();
-    ParallelTransition pt = new ParallelTransition ();
-    
-
     
     public static void main(String[] args) 
     {
         launch(args);
     }
     
+    /** 
+     * start method will create the target and the start button first 
+     * displayed on-screen to the user 
+     */
     @Override
     public void start(Stage primaryStage) 
     {
@@ -72,98 +74,86 @@ public class BullsEye extends Application
     public void animation()
     {
         ScaleTransition[] st = new ScaleTransition[7];
+        boolean recycleCircles = false;
+        
         
         st[0]= new ScaleTransition(Duration.seconds(7), cir[0]);
-        //st[0].setCycleCount(Timeline.INDEFINITE);
         st[0].setToX(0.0f);
         st[0].setToY(0.0f);
         st[0].play();
         
         st[1] = new ScaleTransition(Duration.seconds(5.5), cir[1]);
-        //st[1].setCycleCount(Timeline.INDEFINITE);
         st[1].setToX(0.0f);
         st[1].setToY(0.0f);
         st[1].play();
         
         st[2] = new ScaleTransition(Duration.seconds(4), cir[2]);
-        //st[2].setCycleCount(Timeline.INDEFINITE);
         st[2].setToX(0.0f);
         st[2].setToY(0.0f);
         st[2].play();
         
        // int delayInc = 1;
         int delay = 1;
-      
-        
-        //will create circles (1st white, then red) and add to scaleTransitions
-        for(int i = 3; i<st.length; i++)
-        {     
-            if(i % 2 == 1)
-            {
-                cir[i] = new Circle(400,250,200);
-                cir[i].setFill(Color.WHITE);
-                cir[i].setStyle("-fx-border-color:black;");
-                root.getChildren().add(cir[i]);
-                cir[i].toBack();
-                
-                st[i] = new ScaleTransition(Duration.seconds(7), cir[i]);
-                st[i].setCycleCount(Timeline.INDEFINITE);
-
-                //====may or may not need later
-                //st[i].setDelay(Duration.seconds(delay));
-                //delay++;
-                st[i].setToX(0.0f);
-                st[i].setToY(0.0f);
-            }
-            
-            else if(i%2==0)
-            {
-                cir[i] = new Circle(400, 250, 200);
-                cir[i].setFill(Color.RED);
-                cir[i].setStyle("-fx-border-color:black;");
-                root.getChildren().add(cir[i]);
-                cir[i].toBack();
-                              
-                st[i] = new ScaleTransition(Duration.seconds(7), cir[i]);
-                st[i].setCycleCount(Timeline.INDEFINITE);
-                //=======may or may not need later on
-                //st[i].setDelay(Duration.seconds(delay));
-                //delay++;
-                st[i].setToX(0.0f);
-                st[i].setToY(0.0f);
-            }
-            
-            
-            
-        }
-        
-        //add all the scale transitions to pt 
-        for(int i = 0; i<7; i++)
+              
+        //will create circles (will rotate between white and red) and then add 
+        //to scaleTransitions
+        while(btn.isPressed() == false)
         {
-            pt.getChildren().add(st[i]);
-        } 
-        
-        pt.play();
-        
-        
-        
-        
-        
+            for(int i = 3; i<st.length; i++)
+            {     
+                if(recycleCircles == true)
+                {
+                    i = 0;
+                    recycleCircles = false;
+                }
+                if(i % 2 == 1)
+                {
+                    cir[i] = new Circle(400,250,200);
+                    cir[i].setFill(Color.WHITE);
+                    cir[i].setStyle("-fx-border-color:black;");
+                    root.getChildren().add(cir[i]);
+                    cir[i].toBack();
+
+                    st[i] = new ScaleTransition(Duration.seconds(7), cir[i]);
+                    st[i].setDelay(Duration.seconds(delay));
+                    delay++;
+                    st[i].setToX(0.0f);
+                    st[i].setToY(0.0f);
+                    st[i].play();
+
+                }
+
+                else if(i%2==0)
+                {
+                    cir[i] = new Circle(400, 250, 200);
+                    cir[i].setFill(Color.RED);
+                    cir[i].setStyle("-fx-border-color:black;");
+                    root.getChildren().add(cir[i]);
+                    cir[i].toBack();
+
+                    st[i] = new ScaleTransition(Duration.seconds(7), cir[i]);
+                    st[i].setDelay(Duration.seconds(delay));
+                    delay++;
+                    st[i].setToX(0.0f);
+                    st[i].setToY(0.0f);
+                    st[i].play();
+                }
+                
+                if(i == 6)
+                    recycleCircles = true;
+            }
+        }
+                       
+        //btn.pressedProperty().addListener(listener);
         
         btn.setOnMousePressed(event ->
-        {
-            for(int x = 0; x<st.length;x++)
-            pt.pause();
-            btn.setText("Release to Resume");
-        });    
-
-        
+        {   
+        });   
+   
         btn.setOnMouseReleased(event -> 
         {
             for(int y = 0; y<st.length;y++)
-            {
-                pt.play();
-                btn.setText("Press to Pause");
+            {       
             }
         });
 
